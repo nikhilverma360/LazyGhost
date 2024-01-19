06a5e67786c6b7c13bc856c19cb2d2b940dd7afb
Nikhil Verma <nv5475609@gmail.com>
2024-01-19 18:07:34 +0530

    fix page imports and buton

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
+"use client";
+import React from "react";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+// import { ConnectKitButton } from 'connectkit';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    <Button>Click ME</Button>
+    <main className="flex bg-[#39375A] min-h-screen flex-col items-center justify-between p-24">
+      <Button>Click ME</Button>
+      {/* <ConnectKitButton /> */}
     </main>
   );
 }
