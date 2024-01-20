0881a16a23d1d6a158eb062423389539feb741ee
Nikhil Verma <nv5475609@gmail.com>
2024-01-20 21:18:56 +0530

    sdk implimentation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,34 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import { Button } from "@/components/ui/button";
+
+import LazyPay from "@/pages/LazyGhostSDK/lazypay";
 import { ConnectKitButton } from "connectkit";
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "@/components/ui/alert-dialog";
-import LazyPay from "@/LazyGhostSDK/lazypay";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen bg-[#39375A] flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <Button>Click ME</Button>
-      <LazyPay/>
-    </main>
+    <div className="bg-[#39375A]">
+      <div className="absolute top-0 left-0 p-4 bg-blue-500 text-white">
+        <ConnectKitButton />
+      </div>
+      <div className=" flex flex-col  items-center">
+      <p className=" text-white text-2xl">Lazy Store </p>
+      </div>
+     
+      <main
+        className={`flex min-h-screen flex-col items-center p-24 ${inter.className}`}
+      >
+        <Image src={'/sneaker.png'} alt="" width={500} height={500}></Image>
+        <br/>
+        <p className=" text-white text-2xl">BUY FOR $1 </p>
+        <br/>
+        
+        <div className="mt-8">
+        <LazyPay />
+        </div>
+       
+      </main>
+    </div>
   );
 }
