120d0a377de23075a64ceeb9ab3a6e42089408bc
Nikhil Verma <nv5475609@gmail.com>
2024-01-20 18:47:46 +0530

    added connect kit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,34 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { WagmiConfig, createConfig } from "wagmi";
+import {
+  ConnectKitProvider,
+  ConnectKitButton,
+  getDefaultConfig,
+} from "connectkit";
+
+const config = createConfig(
+  getDefaultConfig({
+    // Required API Keys
+    alchemyId: "fLyKS73P8j9mbSy8jpASyBR_dD9QQFgg", // or infuraId
+    walletConnectProjectId: "2f1191ba25be821c4c7a6b1d5f6959ed",
+
+    // Required
+    appName: "Your App Name",
+
+    // Optional
+    appDescription: "Your App Description",
+    appUrl: "https://family.co", // your app's url
+    appIcon: "https://family.co/logo.png", // your app's icon, no bigger than 1024x1024px (max. 1MB)
+  })
+);
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <WagmiConfig config={config}>
+      <ConnectKitProvider>
+        <Component {...pageProps} />;
+      </ConnectKitProvider>
+    </WagmiConfig>
+  );
 }
